126687dfe7283510e4da905ca5dc3f3082c0a6cc
Aung Min Oo <aungminoo1314@gmail.com>
2021-09-11 13:09:15 +0900

    project setup complete

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import "./bootstrap";
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        new Vue({
+            render: h => h(App, props),
+        }).$mount(el)
+    },
+})
---
+++ resources/js/pages/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>This is about inertia page</h1>
+</template>
+
+<script>
+export default {
+name: "About.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
